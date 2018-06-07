eab4f1b09dd83a08a42c76199399a54820e6427c
Brian Cortes <briancorts166@gmail.com>
2018-06-07 23:11:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var theBeatlesPlay = (musicians,instruments) => {
 
-  var newArray = []
-
+  var newArray = [];
+  var i;
   for (i = 0; i < musicians.length; i++) {
 
     newArray.push(`${musicians[j]} plays ${instruments[j]}`)
