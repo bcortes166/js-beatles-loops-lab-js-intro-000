5d7c411944e12acc2c8a5fe27cac0b947928712a
Brian Cortes <briancorts166@gmail.com>
2018-06-07 23:10:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var theBeatlesPlay = (musicians,instruments) => {
 
   var newArray = []
-  
+
   for (i = 0; i < musicians.length; i++) {
-    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+
+    var j = i
+    newArray.push(`${musicians[j]} plays ${instruments[j]}`)
   }
 }
